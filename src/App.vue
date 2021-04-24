b476b7381691158ffcaa5b1d850c44e3b70549b9
ianvleeshouwers <72982685+ianvleeshouwers@users.noreply.github.com>
2021-04-24 16:24:09 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img style="margin-left:40%" src="https://user-images.githubusercontent.com/17185462/58276211-aeb40a80-7db8-11e9-9087-ec8a729bc08d.PNG">
+    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+
+export default {
+  name: "App",
+  components: {
+    HelloWorld
+  }
+};
+</script>
+
+<style>
+/* #app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+} */
+</style>
